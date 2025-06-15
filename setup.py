873880edd3b62d4130583d4a2b6a79f4d8fb7e0e
wasAmesha <ameshawahalathanthri20@gmail.com>
2025-06-16 05:09:56 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Advanced AI Project',
+    version= '0.0.0',
+    author= 'Amesha Wahalathanthri',
+    author_email= 'ameshawahalathanthri20@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
