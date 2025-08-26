eca5fa5b72577cbc283200d17cc36e0ffdb1f241
wasAmesha <ameshawahalathanthri20@gmail.com>
2025-08-26 08:04:40 +0530

    corrected the code

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ docsearch = PineconeVectorStore.from_existing_index(
     embedding=embeddings,
 )
 
-retriever = docsearch.as_retriever(search_type="similarity",search_kwags={"k":3})
+retriever = docsearch.as_retriever(search_type="similarity",search_kwargs={"k":3})
 
-llm =OpenAI(temperature=0.4, max_tokens=500)
+llm = OpenAI(temperature=0.4, max_tokens=500)
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
